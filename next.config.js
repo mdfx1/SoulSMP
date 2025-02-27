2337431ed027419bd4abce24dff17cd764a608d2
mdfx1 <wate0@proton.me>
2025-02-27 13:09:14 +0000

    TwT

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  staticImage: true,
+  flexsearch: { codeblocks: false },
+  defaultShowCopyCode: true,
+});
 
-module.exports = withNextra()
+module.exports = withNextra({
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
+  reactStrictMode: true,
+});
---
+++ theme.config.js
@@ -0,0 +1,9 @@
+export default {
+  projectLink: "https://github.com/mdfx1/SoulSMP",
+  docsRepositoryBase: "https://github.com/mdfx1/SoulSMP",
+  titleSuffix: " – SoulSMP",
+  logo: <span>SoulSMP</span>,
+  footer: {
+    text: "© 2025 SoulSMP",
+  },
+};
