df79f60d7d2ad59719d29614b001aa92cd37ab50
mdfx1 <wate0@proton.me>
2025-02-27 12:50:57 +0000

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>SoulSMP</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.gg/adZ7hbZRbw',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'SoulSMP',
+  },
+}
+
+export default config
