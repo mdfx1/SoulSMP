33cbc6a108a10c2a4f62c75cd13c687076bd26f6
mdfx1 <wate0@proton.me>
2025-02-28 00:27:05 +0000

    TwT

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>SoulSMP</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/mdfx1/SoulSMP',
   },
   chat: {
     link: 'https://discord.gg/adZ7hbZRbw',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/mdfx1/SoulSMP',
   footer: {
     text: 'SoulSMP',
   },
