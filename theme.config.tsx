33f9ed5451af11f5acaf8009d023292e5a1ef44d
mdfx1 <wate0@proton.me>
2025-02-28 00:52:40 +0000

    TwT

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>SoulSMP</span>,
   project: {
-    link: 'https://github.com/mdfx1/SoulSMP',
+    link: "https://github.com/mdfx1/SoulSMP",
   },
   chat: {
-    link: 'https://discord.gg/adZ7hbZRbw',
+    link: "https://discord.gg/adZ7hbZRbw",
   },
-  docsRepositoryBase: 'https://github.com/mdfx1/SoulSMP',
+  docsRepositoryBase: "https://github.com/mdfx1/SoulSMP",
   footer: {
-    text: 'SoulSMP',
+    text: "© 2025 SoulSMP",
   },
-}
+  useNextSeoProps() {
+    return {
+      title: "SoulSMP",
+      openGraph: {
+        title: "SoulSMP",
+        description: "Hier findest du alle wichtigen Infos zu SoulSMP.",
+        url: "#",
+        images: [
+          {
+            url: "#",
+            width: 1200,
+            height: 630,
+            alt: "SoulSMP",
+          },
+        ],
+      },
+    };
+  },
+};
 
-export default config
+export default config;
